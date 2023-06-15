f6f119921f5429e61be72b4491b6c26c26f19e80
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-15 15:17:53 +0900

    feat(route): route path 상수선언, error페이지 생성
    as const 를 사용해 route path 상수 선언 , errorPage 생성

---
+++ src/router/ROUTE_PATH.ts
@@ -0,0 +1,7 @@
+const ROUTE_PATH = {
+  COMING_SOON: "coming-soon",
+  HOME: "/",
+  NOW_PLAYING: "now-playing",
+} as const;
+
+export default ROUTE_PATH;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
 import { ComingSoon, Home, NowPlaying } from "../pages";
+import ErrorPage from "../pages/ErrorPage";
+import ROUTE_PATH from "./ROUTE_PATH";
 
 const router = createBrowserRouter([
   {
@@ -12,15 +14,16 @@ const router = createBrowserRouter([
       },
       {
         element: <ComingSoon />,
-        path: "coming-soon",
+        path: ROUTE_PATH.COMING_SOON,
       },
       {
         element: <NowPlaying />,
-        path: "now-playing",
+        path: ROUTE_PATH.NOW_PLAYING,
       },
     ],
     element: <App />,
-    path: "/",
+    errorElement: <ErrorPage />,
+    path: ROUTE_PATH.HOME,
   },
 ]);
 
