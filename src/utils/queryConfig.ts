4a3233f6c7433fc5149f599374e7820e2bdb8011
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-15 18:49:14 +0900

    feat(api): axios 로 교체
    fetch 에서 axios 로 교체

--- src/utils/queryConfig.ts
+++ src/utils/queryConfig.ts
@@ -2,21 +2,10 @@ import type { QueryFunctionContext } from "@tanstack/react-query";
 
 import { QueryClient } from "@tanstack/react-query";
 
-import { BASE_URL } from "../api/apiUrl";
-
-const options = {
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODE5Mjc4OGE4ZTVhZjE1NDU1MDE0OGM4MDIyZTM5NiIsInN1YiI6IjYzZTI5NDQ4NTI4YjJlMDA3ZDVlZmJkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5HaTsJt_yaL9sXGwgjXdaAfzoK7b6Z1OUlm9ejPK2yA",
-    accept: "application/json",
-  },
-  method: "GET",
-};
+import apiClient, { BASE_URL } from "../api";
 
 const defaultQueryFn = async ({ queryKey }: QueryFunctionContext) => {
-  return await (
-    await fetch(`${BASE_URL}${queryKey.join("/")}`, options)
-  ).json();
+  return await apiClient.get(`${BASE_URL}${queryKey.join("/")}`);
 };
 
 const queryClient = new QueryClient({
