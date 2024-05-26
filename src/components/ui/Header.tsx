cdf7ada7b5cc61df0a7656204cd45b248561d5f3
Dam <60846068+j2h30728@users.noreply.github.com>
2024-05-26 14:06:48 +0900

    feat: 탑바 반응형 디자인 추가 [#7]

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,10 +1,12 @@
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import ROUTE_PATH from '../../constants/route';
 import useUser from '../../hooks/auth/useUser';
 import useDarkMode, { DARK_MODE } from '../../hooks/useDarkMode';
+import { media } from '../../styles/media';
 import { MovieListType } from '../../types/movie';
 import scrollTolTop from '../../utils/scrollTolTop';
 
@@ -28,9 +30,13 @@ const Header = () => {
     { condition: isNowPlayingType, navigate: MovieListType.NOW_PLAYING, text: 'NOW PLAYING' },
   ];
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <HeaderContainer>
-      <Logo to={`/`}>DAMFLIX</Logo>
+      <Logo to={`/`}>DAM</Logo>
       <LinkContainer>
         {NavigatorData.map(navigator => (
           <LinkAndCircle key={navigator.navigate} onClick={scrollTolTop}>
@@ -41,19 +47,22 @@ const Header = () => {
           </LinkAndCircle>
         ))}
       </LinkContainer>
-      {isLoggedIn ? (
-        <Navigator as="button" onClick={handleLogout}>
-          로그아웃
-        </Navigator>
-      ) : (
-        <>
-          <Navigator to={`/${ROUTE_PATH.SIGN_IN}`}>로그인</Navigator>/
-          <Navigator to={`/${ROUTE_PATH.SIGN_UP}`}>회원가입</Navigator>
-        </>
-      )}
-      <DarkModeButton onClick={handleToggleDarkMode}>
-        {currentThemeMode === DARK_MODE.DARK ? DARK_MODE.LIGHT : DARK_MODE.DARK}
-      </DarkModeButton>
+      <HamburgerButton onClick={toggleMenu}>☰</HamburgerButton>
+      <MenuList $isOpen={isMenuOpen}>
+        {isLoggedIn ? (
+          <Navigator as="button" onClick={handleLogout}>
+            로그아웃
+          </Navigator>
+        ) : (
+          <>
+            <Navigator to={`/${ROUTE_PATH.SIGN_IN}`}>로그인</Navigator>/
+            <Navigator to={`/${ROUTE_PATH.SIGN_UP}`}>회원가입</Navigator>
+          </>
+        )}
+        <DarkModeButton onClick={handleToggleDarkMode}>
+          {currentThemeMode === DARK_MODE.DARK ? DARK_MODE.LIGHT : DARK_MODE.DARK}
+        </DarkModeButton>
+      </MenuList>
     </HeaderContainer>
   );
 };
@@ -63,9 +72,7 @@ export default Header;
 const HeaderContainer = styled.div`
   z-index: 1;
   display: flex;
-  flex: 1;
-  justify-content: center;
-  gap: 140px;
+  justify-content: space-around;
   align-items: center;
   position: fixed;
   top: 0;
@@ -80,6 +87,9 @@ const Logo = styled(Link)`
   letter-spacing: -5px;
   text-shadow: -2px -2px 0 ${props => props.theme.color.primary}, 2px -2px 0 ${props => props.theme.color.primary},
     -2px 2px 0 ${props => props.theme.color.primary}, 2px 2px 0 ${props => props.theme.color.primary};
+
+  ${media.medium`
+  font-size: 30px`}
 `;
 
 const Navigator = styled(Link)`
@@ -91,6 +101,8 @@ const Navigator = styled(Link)`
   :active {
     color: ${props => props.theme.color.neutral};
   }
+  ${media.medium`
+  font-size: 13px`}
 `;
 
 const DarkModeButton = styled.button`
@@ -98,8 +110,6 @@ const DarkModeButton = styled.button`
   padding: 10px;
   border-radius: 50%;
   width: fit-content;
-  justify-self: end;
-  margin-right: 30px;
   background-color: ${props => props.theme.color.neutral};
   color: ${props => props.theme.color.text};
   transition: 0.2s ease;
@@ -109,11 +119,13 @@ const DarkModeButton = styled.button`
   :active {
     scale: 0.9;
   }
+  ${media.medium`
+  font-size: 13px`}
 `;
 
 const LinkContainer = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 10px;
 `;
 const Circle = styled(motion.span)`
   position: absolute;
@@ -133,4 +145,38 @@ const LinkAndCircle = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
+`;
+const HamburgerButton = styled.button`
+  display: none;
+  background: none;
+  border: none;
+  font-size: 24px;
+  cursor: pointer;
+  color: ${({ theme }) => theme.color.text};
+
+  ${media.large`
+    display:block;
+    
+  `};
+`;
+const MenuList = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  gap: 20px;
+
+  ${props => media.large`
+    ${css`
+      display: ${props.$isOpen ? 'flex' : 'none'};
+      position: absolute;
+      z-index: 30;
+      right: 20px;
+      top: 80px;
+      width: fit-content;
+      padding: 20px 20px;
+      background-color: ${props.theme.color.secondary};
+      border-radius: 10px;
+      flex-direction: column;
+      align-items: center;
+      border: 1px solid ${props.theme.color.text};
+    `}
+  `}
 `;
